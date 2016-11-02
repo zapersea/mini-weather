7938cf4ad1958aa8e04ce391098f6bd07c1c0e11
zapersea <s349502161@sohu.com>
2016-11-02 21:17:58 +0800

    V 0.2

---
+++ app/src/main/java/com/example/zaperocean/miniweather/bean/City.java
@@ -0,0 +1,71 @@
+package com.example.zaperocean.miniweather.bean;
+
+/**
+ * Created by Zaper Ocean on 2016/11/1.
+ */
+public class City {
+    private String province;
+    private String city;
+    private String number;
+    private String firstPY;
+    private String allPY;
+    private String allFristPY;
+
+    public City(String province, String city, String number, String firstPY, String allPY, String allFristPY) {
+        this.province = province;
+        this.city = city;
+        this.number = number;
+        this.firstPY = firstPY;
+        this.allPY = allPY;
+        this.allFristPY = allFristPY;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getFirstPY() {
+        return firstPY;
+    }
+
+    public void setFirstPY(String firstPY) {
+        this.firstPY = firstPY;
+    }
+
+    public String getAllPY() {
+        return allPY;
+    }
+
+    public void setAllPY(String allPY) {
+        this.allPY = allPY;
+    }
+
+    public String getAllFristPY() {
+        return allFristPY;
+    }
+
+    public void setAllFristPY(String allFristPY) {
+        this.allFristPY = allFristPY;
+    }
+
+}
